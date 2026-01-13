d856b9bca4024f4df134ae950e0889c4520dda70
Víctor J. Goyzueta <167643295+victor-goyzueta@users.noreply.github.com>
2026-01-13 13:33:34 +0100

    update

--- src/parse/parse_cub_utils.c
+++ src/parse/parse_cub_utils.c
@@ -75,7 +75,8 @@ static int	check_map_borders(t_map *map)
 			return (-1);
 		if (map->matrix[0][i] == ' ')
 		{
-			if (map->height > 1 && (map->matrix[1][i] == '0' || is_player_char(map->matrix[1][i])))
+			if (map->height > 1 && (map->matrix[1][i] == '0'
+				|| is_player_char(map->matrix[1][i])))
 				return (-1);
 		}
 		if (map->matrix[map->height - 1][i] == '0' ||
@@ -83,7 +84,8 @@ static int	check_map_borders(t_map *map)
 			return (-1);
 		if (map->matrix[map->height - 1][i] == ' ')
 		{
-			if (map->height > 1 && (map->matrix[map->height - 2][i] == '0' || is_player_char(map->matrix[map->height - 2][i])))
+			if (map->height > 1 && (map->matrix[map->height - 2][i] == '0'
+				|| is_player_char(map->matrix[map->height - 2][i])))
 				return (-1);
 		}
 		i++;
@@ -95,7 +97,8 @@ static int	check_map_borders(t_map *map)
 			return (-1);
 		if (map->matrix[i][0] == ' ')
 		{
-			if (map->width > 1 && (map->matrix[i][1] == '0' || is_player_char(map->matrix[i][1])))
+			if (map->width > 1 && (map->matrix[i][1] == '0'
+				|| is_player_char(map->matrix[i][1])))
 				return (-1);
 		}
 		if (map->matrix[i][map->width - 1] == '0' ||
@@ -103,35 +106,13 @@ static int	check_map_borders(t_map *map)
 			return (-1);
 		if (map->matrix[i][map->width - 1] == ' ')
 		{
-			if (map->width > 1 && (map->matrix[i][map->width - 2] == '0' || is_player_char(map->matrix[i][map->width - 2])))
+			if (map->width > 1 && (map->matrix[i][map->width - 2] == '0'
+				|| is_player_char(map->matrix[i][map->width - 2])))
 				return (-1);
 		}
 		i++;
 	}
 	return (0);
-}
-
-static void	flood_fill(t_map map, int y, int x, int *error)
-{
-	if (*error)
-		return ;
-	if (y < 0 || y >= map.height || x < 0 || x >= map.width)
-	{
-		*error = 1;
-		return ;
-	}
-	if (map.cpy_map[y][x] == ' ' || map.cpy_map[y][x] == '\0')
-	{
-		*error = 1;
-		return ;
-	}
-	if (map.cpy_map[y][x] == '1' || map.cpy_map[y][x] == 'F')
-		return ;
-	map.cpy_map[y][x] = 'F';
-	flood_fill(map, y + 1, x, error);
-	flood_fill(map, y - 1, x, error);
-	flood_fill(map, y, x + 1, error);
-	flood_fill(map, y, x - 1, error);
 }
 
 int	map_check(t_cub *cub)
@@ -140,6 +121,8 @@ int	map_check(t_cub *cub)
 	int		pos_y;
 	int		error;
 
+	if (validate_characters(&cub->map) != 1)
+		exit_error("Invalid characters");
 	if (validate_player(&cub->map, &pos_x, &pos_y) != 1)
 		exit_error("Invalid player");
 	if (check_map_borders(&cub->map) != 0)
